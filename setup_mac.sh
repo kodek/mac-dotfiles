dca7b15f7cf4eb895ec3117c4ce5b8c78bf4f8d4
kodek <kodek@eat1337.com>
2021-08-04 21:36:12 -0600

    Ad a bunch of stuff and apps

--- setup_mac.sh
+++ setup_mac.sh
@@ -4,6 +4,7 @@ exit 1
 
 # TODO: Provide actual VIM settings.
 echo "imap jk <Esc>" >> ~/.vimrc
+echo "imap jk <Esc>" >> ~/.ideavimrc
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,6 +12,9 @@ echo "imap jk <Esc>" >> ~/.vimrc
 brew install fzf
 # To install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install
+
+# ZSH Vi Mode
+brew install zsh-vi-mode
 
 # Mac App Store installer
 brew install mas
@@ -34,6 +38,9 @@ CASKS=(
   steermouse
   bettertouchtool
   divvy
+  goland
+  visual-studio-code
+  istat-menus
 )
 
 brew install --cask ${CASKS[@]}
@@ -46,6 +53,7 @@ brew install --cask ${CASKS[@]}
 defaults write -g ApplePressAndHoldEnabled -bool false
 # Fast key repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain IniitalKeyRepeat -int 35
 
 # Disable smooth scrolling
 defaults write -g NSScrollAnimationEnabled -bool false
