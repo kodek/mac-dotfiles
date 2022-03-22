3833fb01138a62b81d7a9e068fe9fe58f72ce94c
Hernan Cosi <hernan@hernancosi.com>
2022-03-22 12:08:35 -0700

    Add displayplacer helper script

---
+++ dual_monitors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+displayplacer \
+  "id:86C4E26F-88FF-449C-95B8-3D7AF371F5A3 \
+     res:2560x1440 \
+     scaling:on \
+     origin:(0,0) \
+     degree:0" \
+   "id:110CC005-BF97-4E1E-AE82-BAB8CFC7C33F \
+      res:2560x1440
+      scaling:on \
+      origin:(2560,0) \
+      degree:0"
